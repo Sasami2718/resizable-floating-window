942ad7ab69d115601539a9e2837d37a0e308dcec
Sasami2718 <sasami2008github@outlook.com>
2022-06-23 16:47:17 +0800

    fix: update dist

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "resizable-floating-window",
+  publicPath: "/resizable-floating-window/",
 })
